2be5f51a9b8dcd447791a970c5738aba307aa207
6506022630058 <s6506022630058@email.kmutnb.ac.th>
2023-07-18 10:56:57 +0700

    u

---
+++ L03/quiz1.js
@@ -0,0 +1,24 @@
+let values1 = ["Apple",1,false];
+let values2 = ["Fries",9,true];
+let values3 = ["Mars",9,"Apple"];
+
+function compareArrays(arr1,arr2){
+    result = []
+    for (let data1 of arr1) {
+        for (let data2 of arr2) {
+            if (data1 === data2) {
+                result.push(data1);
+            }
+        }
+    } 
+    if (result.length === 0) {
+        result = "No common elements";
+    }else{
+        result = result.join(",");
+    }
+    console.log(result);
+}
+
+compareArrays(values1,values2);
+compareArrays(values1,values3);
+compareArrays(values2,values3);
---
+++ L03/quiz2.js
@@ -0,0 +1,11 @@
+let furniture = ["Table", "Chairs", "Couch"];
+
+function displaynaewtung(arr){
+    for (let str of arr){
+        for (let char of str){
+            console.log(char);
+        }
+    }
+}
+
+displaynaewtung(furniture);
