ce0cdce2e8f46a5af7c8955e4de2bdaf4634df42
6506022630058 <s6506022630058@email.kmutnb.ac.th>
2023-07-25 08:55:08 +0700

    u

---
+++ L04/mainHello.js
@@ -0,0 +1,14 @@
+const hello = require('./libHello');
+
+hello.sayHello();
+console.log(hello.person.name);
+console.log(hello.cube(3));
+console.log(hello.add(5,4));
+console.log(hello.status);
+
+// hello.status = false;
+// console.log(hello.status);
+
+status = false;
+console.log(status);
+console.log(hello.status);
