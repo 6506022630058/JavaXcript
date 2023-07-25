04d86ad174738c116d0b7482c8fd448e5e17a627
6506022630058 <s6506022630058@email.kmutnb.ac.th>
2023-07-25 08:03:04 +0700

    u

---
+++ L04/class.js
@@ -0,0 +1,11 @@
+class Song {
+    constructor(n) {
+        this.title;
+        this.author;
+}
+    play() {
+        console.log("Song playing!");
+    }       
+}
+const mySong = new Song();
+mySong.play();
---
+++ L04/extendclass.js
@@ -0,0 +1,21 @@
+class media {
+    constructor(info) {
+        this.publishDate = info.publishDate;
+        this.name = info.name;
+    }
+}
+
+class Song extends media {
+    constructor(songData) {
+        super(songData);
+        this.artist = songData.artist;
+    }
+}
+
+const mySong = new Song({
+    artist: "Queen",
+    name: "Bohemian Rhapsody",
+    publishDate: 1975,
+});
+
+console.log(mySong.name);
