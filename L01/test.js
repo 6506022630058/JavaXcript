969a14ec4808cce232f07034eb5f708e1ce4f35b
6506022630058 <s6506022630058@email.kmutnb.ac.th>
2023-07-25 11:10:54 +0700

    u

--- L01/test.js
+++ L01/test.js
@@ -1,9 +1,23 @@
-function deleteZero(str) {
-    num = parseFloat(str);
-    return num.toString();
-}
+//Program to remove leading and trailing zeros from an input string
+//example: "230.500" -> "230.5"
+//example: "00402" -> "402"
+//example: "03.1600" -> "3.16"
+//example: "00.00" -> "0"
 
-console.log(deleteZero("230.500"));
-console.log(deleteZero("00402"));
-console.log(deleteZero("03.1600"));
-console.log(deleteZero("00.00"));
+const rl = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+// function deleteZero(str) {
+//     str1 = str
+//     num = parseFloat(str);
+//     num = num.toString();
+//     console.log('The input string is' ,str1,' output is', num);
+// }
+
+rl.question('Enter number: ', (answer) => {
+    num = parseFloat(answer)
+    str = num.toString()
+    console.log('The input string is' ,answer,'output is', str);
+});
