738138e7f53d2d9ad3b9566809210eb56644cf88
6506022630058 <s6506022630058@email.kmutnb.ac.th>
2023-09-12 16:44:15 +0700

    u

--- exam1/test.js
+++ exam1/test.js
@@ -1,3 +1,44 @@
-for (let char of "test"){
-    console.log(char); 
-}// t  e  s  t
+function maxProductExplanation(n) {
+    const input = n;
+    let arr = [];
+    while(n > 0){
+        if (n > 3){
+            n -= 3;
+            arr.push(3);
+        }else if (n > 2){
+            if (n == 3 && arr[0] !== undefined){
+                n -= 3;
+                arr.push(3);
+            }else{
+                n -= 2;
+                arr.push(2);
+            }
+        }else if (n >= 1){
+            if (n == 2 && arr[0] !== undefined){
+                n -= 2;
+                arr.push(2);
+            }else{
+                n -= 1;
+                arr.push(1);
+            }
+        }
+    }
+    if (arr[0] == 3 && arr[arr.length-1] == 1){
+        arr[0] = 2;
+        arr[arr.length-1] = 2;
+    }
+    if (arr.length > 2 && arr.includes(1) && arr.includes(2)){
+        arr.splice(arr.indexOf(1), 1);
+        arr[arr.indexOf(2)] = 3;
+    }
+    arr = arr.sort(function(a, b){return a-b});
+    return `Input: n = ${input}
+Output: ${arr.reduce((a, b) => a * b, 1)}
+Explanation: ${input} = ${arr.join(' + ')}, ${arr.join(' × ')} = ${arr.reduce((a, b) => a * b, 1)}`
+}
+// Example usage
+console.log(maxProductExplanation(2));
+console.log(maxProductExplanation(5));
+console.log(maxProductExplanation(7));
+console.log(maxProductExplanation(10));
+console.log(maxProductExplanation(15));
---
+++ exam1/test.py
@@ -0,0 +1,34 @@
+def maxProductExplanation(n):
+    input = n
+    arr = []
+    while n > 0:
+        if n > 3:
+            n -= 3
+            arr.append(3)
+        elif n > 2:
+            if n == 3 and arr != []:
+                n -= 3
+                arr.append(3)
+            else:
+                n -= 2
+                arr.append(2)
+        elif n >= 1:
+            if n == 2 and arr != []:
+                n -= 2
+                arr.append(2)
+            else:
+                n -= 1
+                arr.append(1)
+    if arr[0] == 3 and arr[len(arr)-1] == 1:
+        arr[0] = 2
+        arr[len(arr)-1] = 2
+    if len(arr) > 2 and 1 in arr and 2 in arr:
+        arr.splice(arr.indexOf(1), 1)
+        arr[arr.indexOf(2)] = 3
+    return arr
+
+print(maxProductExplanation(2))
+print(maxProductExplanation(5))
+print(maxProductExplanation(7))
+print(maxProductExplanation(10))
+print(maxProductExplanation(15))
